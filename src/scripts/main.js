c7653f221fe7ea84ef4161e2ca2e5f6d6112c0cb
EduardoAlphonse <eduardo.afoli@gmail.com>
2021-07-17 21:27:32 -0300

    improving styles; saving e-mail to localStorage

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,23 @@ function saveLead(event) {
 
 	const input = document.querySelector('input[name=email]');
 
-	email = input.value;
+	const isValid = validateEmail(input.value);
 
-	alert(email);
+	if (!isValid) {
+		const errorMessage = document.querySelector('.error');
+
+		errorMessage.classList.add('show-error');
+		setTimeout(() => {
+			errorMessage.classList.remove('show-error');
+		}, 3600);
+
+		return;
+	}
+
+	localStorage.setItem('email_address', input.value);
+}
+
+function validateEmail(email) {
+	const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
 }
